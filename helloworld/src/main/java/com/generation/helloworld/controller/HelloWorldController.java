2cb0a62b3288f03f4f06b3951c0ca7901d58f40c
unknown <ellenmayarap@gmail.com>
2024-05-20 12:33:09 -0300

    Exercício Insomnia

---
+++ helloworld/src/main/java/com/generation/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,32 @@
+package com.generation.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ola-mundo")
+public class HelloWorldController {
+	@GetMapping
+    public String ola() {
+        return "Olá mundo! Bom dia!";
+    }
+    @GetMapping("comidas")
+    public String comidasFavoritas() {
+        return "sushi \nHamburguer";
+    }
+    @GetMapping("livros")
+    public String livrosFavoritos() {
+        return "Eragon \nSenhor dos Aneis";
+    }
+    @GetMapping("bsm")
+    public String bsmGeneration() {
+    	return "Persistência \nOrientação ao Detalhe\nOrientação ao Futuro\nResponsabilidade Pessoal\n"
+    			+ "Trabalho em Equipe\nProatividade\nComunicação";
+    }
+    @GetMapping("objetivos")
+    public String objetivosGen() {
+    	return "Comunição\nMelhorar habilidades técnicas\nRalizar mais exercícios em Spring e MySQL";
+    }
+}
+   
